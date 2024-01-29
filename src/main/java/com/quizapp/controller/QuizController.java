980f40d3479b4d73ce88b603667b5f35574f2721
vishwasransing <vishwasransing@gmail.com>
2024-01-29 11:02:06 +0530

    Added View
    -Added Home Page

--- src/main/java/com/quizapp/controller/QuizController.java
+++ src/main/java/com/quizapp/controller/QuizController.java
@@ -2,15 +2,13 @@ package com.quizapp.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class QuizController {
 	
-	@RequestMapping("/")
-	@ResponseBody
-	public String home() {
-		return "Home";
+	@RequestMapping("/home")
+	public String goToHomePage() {
+		return "index";
 	}
 	
 }
